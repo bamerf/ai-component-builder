f12f13cad2010fe1d5e763afd18c9643abf663b0
Bamdad Erfanian <erfanianbamdad@gmail.com>
2023-06-20 16:18:06 -0400

    add openai integration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { useChat } from 'ai/svelte';
+
+	const { messages, handleSubmit, input } = useChat({
+		api: '/server'
+	});
+</script>
+
+<ul>
+	{#each $messages as message}
+		<li>{message.role} says... {message.content}</li>
+	{/each}
+</ul>
+
+<form on:submit={handleSubmit} method="post">
+	<input bind:value={$input} type="text" />
+	<button type="submit">Generate</button>
+</form>
---
+++ src/routes/server/+server.ts
@@ -0,0 +1,38 @@
+import type { RequestHandler } from './$types';
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+import { OPENAI_KEY } from '$env/static/private';
+
+const config = new Configuration({
+	apiKey: OPENAI_KEY
+});
+
+const openai = new OpenAIApi(config);
+
+export const POST: RequestHandler = async ({ request }) => {
+	const { messages } = await request.json();
+
+	try {
+		const response = await openai.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			stream: true,
+			max_tokens: 7,
+			temperature: 0,
+			messages
+		});
+
+		const stream = OpenAIStream(response);
+
+		return new StreamingTextResponse(stream);
+	} catch (error) {
+		console.error(error);
+
+		return new Response(JSON.stringify(error), {
+			status: 400,
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+	}
+};
