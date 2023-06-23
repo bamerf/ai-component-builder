6c2031d637cf28d79ae961adb53d65826ce10369
Bamdad Erfanian <erfanianbamdad@gmail.com>
2023-06-23 17:22:46 -0400

    add reset button

---
+++ src/components/ResetIcon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let handleReset: () => void;
+</script>
+
+<span on:click={handleReset} on:keypress={handleReset}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="20"
+		height="20"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="white"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="cursor-pointer hover:stroke-neutral-300 feather feather-rotate-ccw"
+		><polyline points="1 4 1 10 7 10" /><path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10" /></svg
+	>
+</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,36 @@
 <script lang="ts">
 	import { useChat } from 'ai/svelte';
 	import { writable, derived } from 'svelte/store';
+	import type { Writable } from 'svelte/store';
 	import clsx from 'clsx';
 	import Highlight, { LineNumbers } from 'svelte-highlight';
 	import typescript from 'svelte-highlight/languages/typescript';
 	import github from 'svelte-highlight/styles/github-dark';
+
 	import '../app.css';
 
+	import ResetIcon from '../components/ResetIcon.svelte';
 	import Button from '../components/Button.svelte';
 	import Control from '../components/Control.svelte';
 	import Toggle from '../components/Toggle.svelte';
 
-	let paddingX = writable(14);
-	let paddingY = writable(8);
-	let backgroundColor = writable('#52525b');
-	let textColor = writable('#FFFFFF');
-	let borderRadius = writable(4);
-	let text = writable('Button');
-	let isBold = writable(false);
+	let originalValues = {
+		paddingX: 14,
+		paddingY: 8,
+		backgroundColor: '#52525b',
+		textColor: '#FFFFFF',
+		borderRadius: 4,
+		text: 'Button',
+		isBold: false
+	};
+
+	let paddingX = writable(originalValues.paddingX);
+	let paddingY = writable(originalValues.paddingY);
+	let backgroundColor = writable(originalValues.backgroundColor);
+	let textColor = writable(originalValues.textColor);
+	let borderRadius = writable(originalValues.borderRadius);
+	let text = writable(originalValues.text);
+	let isBold = writable(originalValues.isBold);
 
 	let combinedValues = derived(
 		[paddingX, paddingY, backgroundColor, textColor, borderRadius, text, isBold],
@@ -55,6 +68,12 @@
 	});
 
 	const bgStyles = 'bg-neutral-900 border border-neutral-400 rounded-md';
+
+	function resetInput<T>(target: Writable<T>, key: keyof typeof originalValues) {
+		if (key in originalValues) {
+			target.update((value) => originalValues[key] as T);
+		}
+	}
 </script>
 
 <svelte:head>
@@ -64,38 +83,55 @@
 <main class="flex gap-4 p-4">
 	<div class={clsx('min-h-[1000px] min-w-[300px] p-4 flex flex-col gap-6', bgStyles)}>
 		<Control label="Padding Horizontal">
-			<div class="flex gap-2">
-				<input class="w-full" type="range" min={2} max={48} step={1} bind:value={$paddingX} />
+			<div class="flex items-center gap-2">
+				<ResetIcon handleReset={() => resetInput(paddingX, 'paddingX')} />
+				<input
+					class="flex-grow w-full"
+					type="range"
+					min={2}
+					max={48}
+					step={1}
+					bind:value={$paddingX}
+				/>
 				<p class="text-white">{$paddingX}</p>
 			</div>
 		</Control>
 
 		<Control label="Padding Vertical">
-			<div class="flex gap-2">
+			<div class="flex items-center gap-2">
+				<ResetIcon handleReset={() => resetInput(paddingY, 'paddingY')} />
 				<input class="w-full" type="range" min={2} max={48} step={1} bind:value={$paddingY} />
 				<p class="text-white">{$paddingY}</p>
 			</div>
 		</Control>
 
 		<Control label="Border Radius">
-			<div class="flex gap-2">
+			<div class="flex items-center gap-2">
+				<ResetIcon handleReset={() => resetInput(borderRadius, 'borderRadius')} />
 				<input class="w-full" type="range" min={0} max={50} step={1} bind:value={$borderRadius} />
 				<p class="text-white">{$borderRadius}</p>
 			</div>
 		</Control>
 
 		<Control label="Button Text">
-			<div class="flex gap-2">
+			<div class="flex items-center gap-2">
+				<ResetIcon handleReset={() => resetInput(text, 'text')} />
 				<input class="w-full" type="text" bind:value={$text} placeholder="Button" />
 			</div>
 		</Control>
 
 		<Control label="Background Color">
-			<input class="w-full bg-neutral-900" type="color" bind:value={$backgroundColor} />
+			<div class="flex items-center gap-2">
+				<ResetIcon handleReset={() => resetInput(backgroundColor, 'backgroundColor')} />
+				<input class="w-full bg-neutral-900" type="color" bind:value={$backgroundColor} />
+			</div>
 		</Control>
 
 		<Control label="Text Color">
-			<input class="w-full bg-neutral-900" type="color" bind:value={$textColor} />
+			<div class="flex items-center gap-2">
+				<ResetIcon handleReset={() => resetInput(textColor, 'textColor')} />
+				<input class="w-full bg-neutral-900" type="color" bind:value={$textColor} />
+			</div>
 		</Control>
 
 		<Control label="Bold">
